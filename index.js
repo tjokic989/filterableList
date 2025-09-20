114328853a8ea6707fde43c28f60772c36a19b23
tjokic989 <tomajokic1989@gmail.com>
2025-09-20 15:04:02 +0200

    I added functionality to js file

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 // Filterable list
 
 const filterInput = document.getElementById("filterInput");
-const filterList = document.querySelectorAll("#filterList, li");
+const filterList = document.querySelectorAll("li");
+
+filterInput.addEventListener('input', () =>{
+    const text = filterInput.value.toLowerCase();
+    filterList.forEach(li => {
+        li.style.display = li.textContent.toLowerCase().includes(text) ? '' : 'none'
+    });
+});
